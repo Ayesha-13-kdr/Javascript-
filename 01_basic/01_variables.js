ad273cf1a15a0fa61439db9ffcbeb21b5d695a4c
Ayesha-13-kdr <139006043+Ayesha-13-kdr@users.noreply.github.com>
2025-11-30 14:10:23 +0000

    done

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let accountEmail = "ayesha@google.com"
+var accountPass = "1234"
+accountCity = "Jaipur"
+let accountState;
+
+accountEmail = "muzi@google.com"
+//accountId = 23
+accountPass = "0000"
+accountCity = "mumbai"
+
+console.table([accountEmail, accountId, accountPass , accountState, accountCity])
---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,11 @@
+"use strict";  //treat code as newer version  onf js
+
+//alert(3+3) // we are using node js not brower
+
+console.log(3
+
+    +
+    3)// maintain code readebility 
+
+console.log(typeof undefined)
+
