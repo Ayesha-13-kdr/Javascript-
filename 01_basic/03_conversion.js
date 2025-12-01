968b3565e91ef44f9f0d1bc6aa756be2f89eb893
Ayesha-13-kdr <139006043+Ayesha-13-kdr@users.noreply.github.com>
2025-12-01 10:30:50 +0000

    conversion

---
+++ 01_basic/03_conversion.js
@@ -0,0 +1,27 @@
+let score = null
+
+console.log(typeof (score))
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log( valueInNumber)
+
+//"33" 33
+//"33aa" NaN
+//true 1 false 0
+
+let isLoggedIn = undefined
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(typeof booleanIsLoggedIn)
+console.log(booleanIsLoggedIn)
+
+//1 true 0 false
+//"" false
+//"ayesha" true
+//null false
+//undefined false
+
+let someNumber = 93
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber)
+console.log(stringNumber)
